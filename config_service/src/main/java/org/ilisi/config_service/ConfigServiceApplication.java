f6c69edcb0c3cfc4ea204c47e1be2174e44d3eab
Meriem oulja <meriemoulja@gmail.com>
2024-12-01 10:50:26 +0000

    Gateway updated

--- config_service/src/main/java/org/ilisi/config_service/ConfigServiceApplication.java
+++ config_service/src/main/java/org/ilisi/config_service/ConfigServiceApplication.java
@@ -1,6 +1,6 @@
 package org.ilisi.config_service;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
